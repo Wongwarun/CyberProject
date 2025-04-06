44f412f50ded8d95ae7969db7b57ca080d046182
Suphaphon Anukun <s6602041510076@email.kmutnb.ac.th>
2025-04-07 01:47:22 +0700

    init commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,23 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCNi2pkZH7iOjW2geI5nX8E2x-pSFGUx-8",
-  authDomain: "e-learning-e552d.firebaseapp.com",
-  projectId: "e-learning-e552d",
-  storageBucket: "e-learning-e552d.firebasestorage.app",
-  messagingSenderId: "567227709574",
-  appId: "1:567227709574:web:c06b1a92042669c7e1f907",
-  measurementId: "G-LNXFPH8K6F"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
